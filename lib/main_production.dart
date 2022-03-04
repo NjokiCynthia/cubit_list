8c8aa897ec240edaceb4a55938fcb3cb4bb6c219
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-03-04 09:17:56 +0300

    First commit

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:the_list/app/app.dart';
+import 'package:the_list/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
