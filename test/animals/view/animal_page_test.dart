2eb044d2ae2bf16eb5d165715e6d6e338e452b6e
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-03-05 15:11:30 +0300

    Created the animal model

--- test/animals/view/animal_page_test.dart
+++ test/animals/view/animal_page_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import 'package:the_list/counter/counter.dart';
+import 'package:the_list/counter/animals.dart';
 
 import '../../helpers/helpers.dart';
 
